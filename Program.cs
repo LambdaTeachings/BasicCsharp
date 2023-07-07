1f7d4fe55ba762f0d3b1dd3cab1cee1a1e809c62
MisterKidX <agesonera@gmail.com>
2023-07-07 15:26:48 +0300

    added assignment description

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * demo - there be monsters.
+ * As technical designers on a new upcoming dungeon crawler, we were tasked with writing a simple
+ * algorithm that will calculate the threat level of an encounter with monsters.The algorithm’s variables are:
+    * player levels
+    * player count
+    * monsters’ hit points
+    * monsters’ damage
+    * monster count
+ * The output of the algorithm is a dangerLevel : string with the following options: Easy, Medium, Hard and Impossible.
+ */
+
+int monster1HP;
+int monster2HP;
+int monster3HP;
+
+int monster1Damage;
+int monster2Damage;
+int monster3Damage;
+
