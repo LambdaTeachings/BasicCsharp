6284f449309b70f4cf40b002af627684e77c6c66
MisterKidX <agesonera@gmail.com>
2023-07-11 21:11:23 +0300

    added code sample

--- Program.cs
+++ Program.cs
@@ -1,76 +1,51 @@
-﻿/*
- * demo - there be monsters.
- * As technical designers on a new upcoming dungeon crawler, we were tasked with writing a simple
- * algorithm that will calculate the threat level of an encounter with monsters.The algorithm’s variables are:
-    * player levels
-    * player count
-    * monsters’ hit points
-    * monsters’ damage
-    * monster count
- * The output of the algorithm is a dangerLevel : string with the following options: Easy, Medium, Hard and Impossible
- * (this will be presented to the players in the game).
- */
+﻿#if UNITY_EDITOR
 
-// players data
-int player1Level = 9;
-int player2Level = 12;
+using UnityEngine;
 
-// mosnters data
-int monster1HP = 10;
-int monster2HP = 20;
-int monster3HP = 80;
-
-int monster1Damage = 60;
-int monster2Damage = 40;
-int monster3Damage = 10;
-
-// Encounter data
-int easy = -1;
-int medium = 3;
-int hard = 8;
-
-// players calculations
-int playerLevels = player1Level + player2Level;
-int playerCount = 2;
-
-float playersCompentence = playerLevels * (1 + playerCount / 10f);
-
-// mosnters calculations
-int monsterCount = 3;
-
-float monster1ThreatLevel = (monster1HP + monster1Damage) / 10f;
-float monster2ThreatLevel = (monster2HP + monster2Damage) / 10f;
-float monster3ThreatLevel = (monster3HP + monster3Damage) / 10f;
-
-float encounterThreatLevel = (monster1ThreatLevel + monster2ThreatLevel +
-    monster3ThreatLevel) * (1 + monsterCount / 10f);
-
-// data output
-
-Console.WriteLine("There are " + playerCount + " players, fighting " + monsterCount + " monsters.");
-
-// encounter threat output
-float deltaThreat = encounterThreatLevel - playersCompentence;
-
-if (deltaThreat <= easy)
+public class CreateMinecraftWorld : MonoBehaviour
 {
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Easy Encounter");
-}
-else if (deltaThreat <= medium)
-{
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("Medium Encounter");
-}
-else if (deltaThreat <= hard)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Hard Encounter");
-}
-else
-{
-    Console.ForegroundColor = ConsoleColor.DarkRed;
-    Console.WriteLine("Impossible Encounter");
-}
+    public int height = 8;
+    public int xyDimenstions = 50;
+    public float Resolution = 1f;
+    public float seed = 0;
 
-Console.ResetColor();
+    GameObject _container;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        if (_container != null)
+        {
+            Destroy(_container);
+        }
+
+        _container = new GameObject("minecraft");
+
+        for (int i = 0; i < xyDimenstions; i++)
+        {
+            for (int j = 0; j < xyDimenstions; j++)
+            {
+                var perlinX = ((float)i / xyDimenstions + seed) * Resolution;
+                var perlinY = ((float)j / xyDimenstions + seed) * Resolution;
+
+                var noise = Mathf.PerlinNoise(perlinX, perlinY) * height;
+
+                for (int k = 0; k < noise; k++)
+                {
+                    var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(i, k, j);
+
+                    cube.transform.SetParent(_container.transform);
+                }
+            }
+        }
+    }
+}
+#else
+Console.WriteLine("You need to run this code inside Unity.");
+#endif
