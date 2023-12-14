d302abe44347b67a9e740779571e75b5bedb3615
Dor Ben Dor <agesonera@gmail.com>
2023-12-14 16:08:43 +0200

    init

--- Program.cs
+++ Program.cs
@@ -1,32 +1,117 @@
-﻿using RPG;
-using RPG.Factories;
+﻿using BasicCsharp;
+using System.Drawing;
+using System.Resources;
 
-var weapon = new Weapon() { Name = "Plain Sword", Description = "A Plain Sword dude", Damage = 11, HitChance = 0.8f };
-var armor = new Armor() { Name = "Gilded Breeches", AbsoluteDamageReduction = 1, EvasionChance = 0.1f, RelativeDamageReduction = 0.2f };
-var player = new CombatEntity(5) { Name = "Player", Weapon = weapon, Armor = armor };
+var colors = GetColorsFromText(imageText.Image);
 
-while (!player.IsDead)
+Downsample(colors, 4);
+
+// This is the algorithm
+Bitmap Downsample(Color[,] colors, int factor)
 {
-    var opponent = EnemyFactory.ConstructEnemy();
-    Console.WriteLine($"You encounter a {opponent.Name}!");
+    var width = (int)Math.Ceiling((float)colors.GetLength(0) / factor);
+    var height = (int)Math.Ceiling((float)colors.GetLength(1) / factor);
+    Bitmap output = new Bitmap(width, height);
 
-    while (!opponent.IsDead && !player.IsDead)
+    for (int i = 1; i < colors.GetLength(0) - 1; i += factor)
     {
-        // opponent turn
+        for (int j = 1; j < colors.GetLength(1) - 1; j += factor)
+        {
+            int r = 0, g = 0, b = 0;
+            Color[] c = new Color[9];
+            c[0] = colors[i, j + 1];
+            c[1] = colors[i, j];
+            c[2] = colors[i, j - 1];
+            c[3] = colors[i + 1, j + 1];
+            c[4] = colors[i + 1, j];
+            c[5] = colors[i + 1, j - 1];
+            c[6] = colors[i - 1, j + 1];
+            c[7] = colors[i - 1, j];
+            c[8] = colors[i - 1, j - 1];
 
-        player.TryAttack(opponent);
-        Console.ReadLine();
-        opponent.TryAttack(player);
-        Console.ReadLine();
-        Console.Clear();
+            var color = Average(c);
+            output.SetPixel(i / factor, j / factor, color);
+        }
     }
-
-    if (player.IsDead)
-        break;
-
-    Console.WriteLine($"You defeated {opponent.Name}!");
-    Console.ReadLine();
-    Console.Clear();
+    var savePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    output.Save(savePath + @$"/cartman_{factor}.jpeg");
+    return output;
 }
 
-Console.WriteLine("Player died, Game Over!");
+Bitmap Reconstruct(Color[,] colors)
+{
+    var width = colors.GetLength(0);
+    var height = colors.GetLength(1);
+    Bitmap output = new Bitmap(width, height);
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            output.SetPixel(i, j, colors[i, j]);
+        }
+    }
+
+    var savePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+    output.Save(savePath + @"/cartman.jpeg");
+    return output;
+}
+
+Color Average(params Color[] colors)
+{
+    int r = 0, g = 0, b = 0;
+    foreach (var item in colors)
+    {
+        r += item.R;
+        g += item.G;
+        b += item.B;
+    }
+    var sum = colors.Length;
+
+    var r1 = (int)((float)r / sum);
+    var g1 = (int)((float)g / sum);
+    var b1 = (int)((float)b / sum);
+    return Color.FromArgb(r1, g1, b1);
+}
+
+Color[,] GetColorsFromText(string path)
+{
+    var txt = File.ReadAllLines(path);
+    var header = txt[0];
+    var w = GetValue(header, "Width");
+    var h = GetValue(header, "Height");
+    var colors = new Color[w, h];
+    var conv = new ColorConverter();
+
+    for (int i = 0; i < w; i++)
+    {
+        for (int j = 0; j < h; j++)
+        {
+            Color color;
+            var index = i * (h - 1) + j;
+            if (index == 0)
+            {
+                index++;
+            }
+            color = (Color)conv.ConvertFromString("#" + txt[index]);
+            colors[i, j] = color;
+        }
+    }
+    return colors;
+}
+
+int GetValue(string line, string tag)
+{
+    var start = line.IndexOf(tag + "=") + tag.Length + 1;
+    var end = start;
+    for (int i = start; i < line.Length; i++)
+    {
+        if (!char.IsDigit(line[i]))
+        {
+            end = i;
+            break;
+        }
+    }
+
+    return int.Parse(line.Substring(start, end - start));
+}
