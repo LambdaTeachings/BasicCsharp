8395d49eeb42ddffd54cab4ff3fbb82b538c6cbd
Dor Ben Dor <agesonera@gmail.com>
2024-01-13 09:24:36 +0200

    added the tree DTS

--- Demo/Graph.cs
+++ Demo/Graph.cs
@@ -1,190 +1,193 @@
 ﻿using System.Collections.ObjectModel;
 
-public class LimitedGraph
+namespace Demo.Graph
 {
-    public int Count { get; private set; }
-    public Node Root { get; set; }
-    HashSet<Node> _context = new();
-
-    public LimitedGraph(int value)
+    public class LimitedGraph
     {
-        var node = new Node(value, this);
-        Root = node;
-        _context.Add(node);
-        Count = 1;
-    }
+        public int Count { get; private set; }
+        public Node Root { get; set; }
+        HashSet<Node> _context = new();
 
-    public Node AddNode(int value)
-    {
-        Count++;
-        var node = new Node(value, this);
-        _context.Add(node);
-        return node;
-    }
-
-    public void RemoveNode(Node node)
-    {
-        node.Remove();
-        _context.Remove(node);
-        Count--;
-    }
-
-    public void ConnectNodes(int edgeValue, Node a, Node b)
-    {
-        if (a.ConnectedTo(b))
-            throw new ArgumentException("Nodes are already connected.");
-        else
+        public LimitedGraph(int value)
         {
-            new Edge(edgeValue, a, b, this);
+            var node = new Node(value, this);
+            Root = node;
+            _context.Add(node);
+            Count = 1;
         }
-    }
 
-    public void Print()
-    {
-        foreach (var node in _context)
+        public Node AddNode(int value)
         {
-            Console.WriteLine(node);
+            Count++;
+            var node = new Node(value, this);
+            _context.Add(node);
+            return node;
         }
-    }
-}
 
-public class Node
-{
-    public int Value { get; set; }
-    public LimitedGraph Graph { get; internal init; }
-
-    public ReadOnlyCollection<Edge> Edges => _edges.AsReadOnly();
-    public ReadOnlyCollection<Node> ConnectedNodes
-    {
-        get
+        public void RemoveNode(Node node)
         {
-            var nodes = new List<Node>();
-            for (int i = 0; i < _edges.Count; i++)
+            node.Remove();
+            _context.Remove(node);
+            Count--;
+        }
+
+        public void ConnectNodes(int edgeValue, Node a, Node b)
+        {
+            if (a.ConnectedTo(b))
+                throw new ArgumentException("Nodes are already connected.");
+            else
             {
-                Edge edge = _edges[i];
-                nodes.Add(edge.GetOther(this));
-            }
-
-            return nodes.AsReadOnly();
-        }
-    }
-
-    private List<Edge> _edges = new List<Edge>();
-
-    internal Node(int value, LimitedGraph graph)
-    {
-        Graph = graph;
-        Value = value;
-    }
-
-    public void ConnectTo(Node node, int edgeValue)
-    {
-        Graph.ConnectNodes(edgeValue, this, node);
-    }
-
-    internal void DisconnectFromEdge(Edge edge)
-    {
-        _edges.Remove(edge);
-    }
-
-    internal void Remove()
-    {
-        foreach (var edge in _edges)
-        {
-            edge.Disconnect();
-        }
-    }
-
-    public bool ConnectedTo(Node node)
-    {
-        foreach (var edge in _edges)
-        {
-            if (edge.GetOther(node) == node)
-            {
-                return true;
+                new Edge(edgeValue, a, b, this);
             }
         }
 
-        return false;
+        public void Print()
+        {
+            foreach (var node in _context)
+            {
+                Console.WriteLine(node);
+            }
+        }
     }
 
-    internal void Connect(Edge edge)
+    public class Node
     {
-        _edges.Add(edge);
+        public int Value { get; set; }
+        public LimitedGraph Graph { get; internal init; }
+
+        public ReadOnlyCollection<Edge> Edges => _edges.AsReadOnly();
+        public ReadOnlyCollection<Node> ConnectedNodes
+        {
+            get
+            {
+                var nodes = new List<Node>();
+                for (int i = 0; i < _edges.Count; i++)
+                {
+                    Edge edge = _edges[i];
+                    nodes.Add(edge.GetOther(this));
+                }
+
+                return nodes.AsReadOnly();
+            }
+        }
+
+        private List<Edge> _edges = new List<Edge>();
+
+        internal Node(int value, LimitedGraph graph)
+        {
+            Graph = graph;
+            Value = value;
+        }
+
+        public void ConnectTo(Node node, int edgeValue)
+        {
+            Graph.ConnectNodes(edgeValue, this, node);
+        }
+
+        internal void DisconnectFromEdge(Edge edge)
+        {
+            _edges.Remove(edge);
+        }
+
+        internal void Remove()
+        {
+            foreach (var edge in _edges)
+            {
+                edge.Disconnect();
+            }
+        }
+
+        public bool ConnectedTo(Node node)
+        {
+            foreach (var edge in _edges)
+            {
+                if (edge.GetOther(node) == node)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        internal void Connect(Edge edge)
+        {
+            _edges.Add(edge);
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} [{_edges.Count}]";
+        }
     }
 
-    public override string ToString()
+    public class Edge
     {
-        return $"{Value} [{_edges.Count}]";
+        public int Value { get; set; }
+        public LimitedGraph Graph { get; internal init; }
+
+        public Node A { get; private set; }
+        public Node B { get; private set; }
+
+        internal Edge(int value, Node a, Node b, LimitedGraph graph)
+        {
+            Value = value;
+            A = a ?? throw new ArgumentNullException(nameof(a));
+            B = b ?? throw new ArgumentNullException(nameof(b));
+            A.Connect(this);
+            B.Connect(this);
+            Graph = graph;
+        }
+
+        internal void Disconnect()
+        {
+            A.DisconnectFromEdge(this);
+            B.DisconnectFromEdge(this);
+            A = null;
+            B = null;
+        }
+
+        internal Node GetOther(Node node)
+        {
+            if (A == node)
+                return B;
+            else if (B == node)
+                return A;
+            else
+                throw new ArgumentException(nameof(node));
+        }
     }
-}
 
-public class Edge
-{
-    public int Value { get; set; }
-    public LimitedGraph Graph { get; internal init; }
-
-    public Node A { get; private set; }
-    public Node B { get; private set; }
-
-    internal Edge(int value, Node a, Node b, LimitedGraph graph)
+    public static class DijkstrasAlgorithmPreparation
     {
-        Value = value;
-        A = a ?? throw new ArgumentNullException(nameof(a));
-        B = b ?? throw new ArgumentNullException(nameof(b));
-        A.Connect(this);
-        B.Connect(this);
-        Graph = graph;
-    }
+        public static void Demo()
+        {
+            LimitedGraph graph = new(0);
 
-    internal void Disconnect()
-    {
-        A.DisconnectFromEdge(this);
-        B.DisconnectFromEdge(this);
-        A = null;
-        B = null;
-    }
+            // a initial
+            // e destination
+            var a = graph.Root;
+            var b = graph.AddNode(int.MaxValue);
+            var c = graph.AddNode(int.MaxValue);
+            var d = graph.AddNode(int.MaxValue);
+            var e = graph.AddNode(int.MaxValue);
+            var f = graph.AddNode(int.MaxValue);
 
-    internal Node GetOther(Node node)
-    {
-        if (A == node)
-            return B;
-        else if (B == node)
-            return A;
-        else
-            throw new ArgumentException(nameof(node));
-    }
-}
+            a.ConnectTo(b, 14);
+            a.ConnectTo(c, 9);
+            a.ConnectTo(d, 7);
 
-public static class DijkstrasAlgorithmPreparation
-{
-    public static void Demo()
-    {
-        LimitedGraph graph = new(0);
+            b.ConnectTo(e, 9);
+            b.ConnectTo(c, 2);
 
-        // a initial
-        // e destination
-        var a = graph.Root;
-        var b = graph.AddNode(int.MaxValue);
-        var c = graph.AddNode(int.MaxValue);
-        var d = graph.AddNode(int.MaxValue);
-        var e = graph.AddNode(int.MaxValue);
-        var f = graph.AddNode(int.MaxValue);
+            c.ConnectTo(d, 10);
+            c.ConnectTo(f, 11);
 
-        a.ConnectTo(b, 14);
-        a.ConnectTo(c, 9);
-        a.ConnectTo(d, 7);
+            d.ConnectTo(f, 15);
 
-        b.ConnectTo(e, 9);
-        b.ConnectTo(c, 2);
+            e.ConnectTo(f, 6);
 
-        c.ConnectTo(d, 10);
-        c.ConnectTo(f, 11);
-
-        d.ConnectTo(f, 15);
-
-        e.ConnectTo(f, 6);
-
-        List<Node> unvisited = new() { a, b, c, d, e, f };
+            List<Node> unvisited = new() { a, b, c, d, e, f };
+        }
     }
 }
---
+++ Demo/Tree.cs
@@ -0,0 +1,68 @@
+﻿public class Tree
+{
+    public int Height { get; private set; }
+    public Node Root { get; init; }
+
+    public Tree(int rootKey)
+    {
+        Root = new Node(rootKey);
+        Height = 0;
+    }
+
+    public Node ConnectChild(Node parent, int key)
+    {
+        var node = new Node(key);
+        parent.ConnectChild(node);
+
+        return node;
+    }
+
+    public void DeleteNode(Node node)
+    {
+        if (node == Root)
+            throw new InvalidOperationException("Cannot delete the root node of a tree.");
+        else
+            node.Delete();
+    }
+}
+
+public class Node
+{
+    public int Key { get; set; }
+    public bool IsRoot => Parent == null;
+    public bool IsLeaf => Children == null || Children.Count == 0;
+    public int Level { get; }
+    public Node Parent { get; private set; }
+
+    // optimally this would be a readonly DTS
+    public List<Node> Children { get; private set; } = new();
+    public List<Node> Siblings => Parent?.Children;
+
+    public Node(int key)
+    {
+        Key = key;
+        if (!IsRoot)
+            Level = Parent.Level + 1;
+    }
+
+    internal void ConnectChild(Node child)
+    {
+        Children.Add(child);
+        child.Parent = this;
+    }
+
+    internal void Delete()
+    {
+        Parent.Children.Remove(this);
+        Parent = null;
+
+        // recursion
+        // To incur garbage collection
+        // limits the size of the tree
+        for (int i = 0; i < Children.Count; i++)
+            Children[i].Delete();
+
+        Children.Clear();
+        Children = null;
+    }
+}
